6aef403f37471beadf79212fe2851a6c4412f539
amaralsilvapaulo-collab <amaral.silva.paulo@escola.pr.gov.br>
2026-07-06 11:29:24 -0300

    p

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
     botoes = document.querySelectorAll("button")
     console.log(botoes)
 
-    botoes.forEach(fuction (botao))
+        botoes.forEach(fuction (botao)) {
     let curtiu = false;
     botao.addEventListener("click" , botaoClicado)
 
     function botaoClicado(){
           console.log("botao clicado")
+          let texto = botao.querySelector("span")
+          if (curtiu == false){
+            texto.textConten++
     }
+  }
+})
